1ba66e2b62be0cef6e83527e73839e8a072568d3
Deborah <69389914+DebbieAUG@users.noreply.github.com>
2026-07-13 06:10:46 +0000

    AppScript Update

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbx4-4sCdujwiAcJglYPc4zfDkMoGVgF9l2Qh62kaR85PLOYjYH0u6Bpj6FYTM1_6sf1/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxfknkYT8L_yBziDdbP35tJe5odjn4jfq4xFdEKWWvA_xksLSr5ZQoTHxeooCg7fF3Z/exec";
 async function login(username, password) {
     const response = await fetch(API_URL, {
         method: "POST",
