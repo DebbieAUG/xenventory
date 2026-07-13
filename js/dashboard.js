070060076ae60a14537cc13462031f625655391c
Deborah <69389914+DebbieAUG@users.noreply.github.com>
2026-07-13 05:58:48 +0000

    P3 Dashboarding

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,47 +6,57 @@ if(role!=="Admin"){
     .getElementById("showAdd")
     .style.display="none";
 }
+
 document
 .getElementById("showInventory")
 .onclick=loadInventory;
 async function loadInventory(){
     const inventory=await getInventory();
     let html = `
-        <table class="w-full bg-white rounded shadow">
-        <thead class="bg-slate-200">
-        <tr>
-        <th class="p-3">ID</th>
-        <th>Item</th>
-        <th>Category</th>
-        <th>Qty</th>
-        <th>Customer</th>
-        <th>Purchase Date</th>
-        <th>Cost</th>
-        <th>Total</th>
-        </tr>
-        </thead>
-        <tbody>
+    <table class="min-w-full">
+    <thead>
+    <tr class="bg-slate-200">
+    <th class="text-left p-3">ID</th>
+    <th class="text-left">Item</th>
+    <th class="text-left">Category</th>
+    <th class="text-left">Qty</th>
+    <th class="text-left">Customer</th>
+    <th class="text-left">Purchase Date</th>
+    <th class="text-left">Cost</th>
+    <th class="text-left">Total</th>
+    </tr>
+    </thead>
+    <tbody>
     `;
     inventory.forEach(item=>{
         html += `
-            <tr class="border-b hover:bg-gray-50">
-            <td class="p-2">${item["Item ID"]}</td>
-            <td>${item["Item Name"]}</td>
-            <td>${item["Category"]}</td>
-            <td>${item["Available Qty"]}</td>
-            <td>${item["Customer"]}</td>
-            <td>${item["Purchase Date"]}</td>
-            <td>₹${item["Cost Per Unit"]}</td>
-            <td>₹${item["Total Cost"]}</td>
-            </tr>
+        <tr class="border-b hover:bg-slate-50">
+        <td class="p-3">${item["Item ID"]}</td>
+        <td>${item["Item Name"]}</td>
+        <td>${item["Category"]}</td>
+        <td>${item["Available Qty"]}</td>
+        <td>${item["Customer"]}</td>
+        <td>${item["Purchase Date"]}</td>
+        <td>₹${item["Cost Per Unit"]}</td>
+        <td>₹${item["Total Cost"]}</td>
+        </tr>
         `;
     });
     html+="</table>";
     document
     .getElementById("inventorySection")
     .innerHTML=html;
+    const totalItems = inventory.length;
+    const totalQty = inventory.reduce(
+        (sum, item) => sum + Number(item["Available Qty"]),
+        0
+    );
+    document.getElementById("cardInventory").innerText = 0;
+    document.getElementById("cardAvailable").innerText = 0;
 }
 loadInventory();
+
+
 
 const modal = document.getElementById("addModal");
 const addBtn = document.getElementById("showAdd");
