e963749e48b3bfab7fc6c9a73c5bb279f66d3c58
Deborah <69389914+DebbieAUG@users.noreply.github.com>
2026-07-15 07:04:21 +0000

    Updates

--- js/returns.js
+++ js/returns.js
@@ -2,9 +2,9 @@ async function loadReturns() {
     const returns = await getReturns();
     renderReturns(returns);
 }
-function renderReturns(returns) {
-    console.log(returns);
-}
+// function renderReturns(returns) {
+//     console.log(returns);
+// }
 async function submitReturnItem(returnData) {
     await submitReturn(returnData);
     await loadDashboard();
